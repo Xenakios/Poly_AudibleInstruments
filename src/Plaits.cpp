4a42d71c107c459bf0925b2a3a033608dab98550
Xenakios <xenakios@gmail.com>
2020-02-28 18:46:36 +0200

    More polyphonic CV handling

--- src/Plaits.cpp
+++ src/Plaits.cpp
@@ -192,18 +192,27 @@ struct Plaits : Module {
 				modulations[i].engine = inputs[ENGINE_INPUT].getVoltage() / 5.f;
 				modulations[i].note = inputs[NOTE_INPUT].getVoltage(i) * 12.f;
 				modulations[i].frequency = inputs[FREQ_INPUT].getVoltage() * 6.f;
-				modulations[i].harmonics = inputs[HARMONICS_INPUT].getVoltage() / 5.f;
-				if (inputs[TIMBRE_INPUT].getChannels()<2)
+				if (inputs[HARMONICS_INPUT].getChannels() < 2)
+					modulations[i].harmonics = inputs[HARMONICS_INPUT].getVoltage() / 5.f;
+				else 
+					modulations[i].harmonics = inputs[HARMONICS_INPUT].getVoltage(i) / 5.f;
+				if (inputs[TIMBRE_INPUT].getChannels() < 2)
 					modulations[i].timbre = inputs[TIMBRE_INPUT].getVoltage() / 8.f;
 				else
 					modulations[i].timbre = inputs[TIMBRE_INPUT].getVoltage(i) / 8.f;
-				modulations[i].morph = inputs[MORPH_INPUT].getVoltage() / 8.f;
+				if (inputs[MORPH_INPUT].getChannels() < 2)
+					modulations[i].morph = inputs[MORPH_INPUT].getVoltage() / 8.f;
+				else
+					modulations[i].morph = inputs[MORPH_INPUT].getVoltage(i) / 8.f;
 				// Triggers at around 0.7 V
-				if (inputs[TRIGGER_INPUT].getChannels()<2)
+				if (inputs[TRIGGER_INPUT].getChannels() < 2)
 					modulations[i].trigger = inputs[TRIGGER_INPUT].getVoltage() / 3.f;
 				else
 					modulations[i].trigger = inputs[TRIGGER_INPUT].getVoltage(i) / 3.f;
-				modulations[i].level = inputs[LEVEL_INPUT].getVoltage() / 8.f;
+				if (inputs[LEVEL_INPUT].getChannels() < 2)
+					modulations[i].level = inputs[LEVEL_INPUT].getVoltage() / 8.f;
+				else
+					modulations[i].level = inputs[LEVEL_INPUT].getVoltage(i) / 8.f;
 
 				modulations[i].frequency_patched = inputs[FREQ_INPUT].isConnected();
 				modulations[i].timbre_patched = inputs[TIMBRE_INPUT].isConnected();
