604f2799b58151ec88fb1ef4806e817506bde1ec
Xenakios <xenakios@gmail.com>
2021-04-01 18:33:21 +0300

    Removed the now redundant Plaits/MO2 module

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,7 +4,6 @@ Plugin *pluginInstance;
 
 void init(Plugin *p) {
 	pluginInstance = p;
-	p->addModel(modelPlaits);
 	p->addModel(modelBraids);
 	p->addModel(modelMarbles);
 }
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -7,7 +7,6 @@ using namespace rack;
 extern Plugin *pluginInstance;
 
 // Declare each Model, defined in each module source file
-extern Model *modelPlaits;
 extern Model *modelBraids;
 extern Model *modelMarbles;
 
